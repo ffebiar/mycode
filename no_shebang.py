18117b23509f3b0592a6512f03a917fe4e2844f0
Felipe Febiar <ffebiar@gmail.com>
2020-10-26 18:16:58 +0000

    Second push

---
+++ no_shebang.py
@@ -0,0 +1,4 @@
+print("Ello!")
+print("Did you just say Hello?")
+print("No, but close enough!")
+
---
+++ with_shebang.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+print("Ello!")
+print("Did you just say Hello?")
+print("No, but close enough!")
+
