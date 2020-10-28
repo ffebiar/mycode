504f0357e0e057760988550ce67b1ad8e03eadc3
Felipe Febiar <ffebiar@gmail.com>
2020-10-28 05:01:41 +0000

    Literaly my entire Tuesday

---
+++ montypython/monty_python7.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+round= 0
+answer= " "
+
+while round < 3 and answer != "brian" and answer != "shrubbery":
+    round += 1
+    answer = input('Finish the movie title, "Monty Python\'s The Life of _____": ').lower()
+    if answer == "shrubbery":
+        print("You gave the super secret answer!")
+        print("Congrats!!")
+    elif answer == "brian":
+        print("Correct!")
+    elif round == 3:
+        print("Sorry, the answer was Brian.")
+    else:
+        print("Sorry. Try again!")
