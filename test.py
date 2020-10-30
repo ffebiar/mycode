a7e79264eace1bb307e4e6be6be9d35b7f14c2a6
Felipe Febiar <ffebiar@gmail.com>
2020-10-30 16:57:08 +0000

    objects

--- test.py
+++ test.py
@@ -1,94 +1,103 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-import os, time
-import sys
-import test2.py
+import time
 
-os.chdir("/home/student/mycode/")
+def main():
 
-ask= 0
-answer= " "
+    ask= 0
+    answer= " "
 
-def R():
-    print("Restarting game...")
-    test2.py()
+    def R():    # This is to restart the game
+        print("Restarting game")
+        time.sleep(0.5)
+        print(".")
+        time.sleep(0.5)
+        print(".")
+        time.sleep(0.5)
+        print(".")
+        main()
     
+    def win():  # Command to run when the script wins
+        print("Thanks for playing!!")
+        print("I hope you had fun!!")
+        answer= input("Do you want to play again? ").lower()
+        if answer == "yes":
+            R()
+        elif answer == "no":
+            bye()
+        else:
+            yon2()
+    
+    def bye():  # Command to run when player does't want to play again after script winning
+        print("Come back again!!")
+        while True:
+            exit()
 
-def win():
-    print("Thanks for playing!!")
-    print("I hope you had fun!!")
-    answer= input("Do you want to play again? ").lower()
-    if answer == "yes":
+    def Q():    # Made for counting the questions
+        print(f"Question {ask}")
+
+    def end():  # Used when player doesn't want to play before the script wins
+        print("Shame, come again next time!!")
+        while True:
+            exit()
+
+    def yon():  # Used when player types anythong other than yes or no
+        print("Sorry, you can only answer Yes or No!")
+
+    def yon2(): # Used after script wins and player still types anything other than yes or no
+        print("This late in the game and you still can\'t follow the rules!!")
+        print("Looks like you\'ll just have to play again!! :D")
         R()
-    elif answer == "no":
-        bye()
-    else:
-        yon2()
-    
-def bye():
-    print("Come back again!!")
-    sys.exit()
 
-def Q():
-    print(f"Question {ask}")
+    while True:
+        print("Welcome!! This is the 20 Questions game!")
+        answer= input("Would you like to play? ").lower()
+        if answer == "yes":
+            print("Great!! Choose between a Lion, Tiger, or Bear!")
+            print("Choose one and I will try and guess what you chose!")
+            break
+        elif answer == "no":
+            end()
+        else:
+            yon()
 
-def end():
-    print("Shame, come again next time!!")
-    sys.exit()
+    while True:         
+        answer = input("Are you ready to play? ").lower()
+        if answer == "yes":
+            print("Awesome, let\'s get started!!")
+            break
+        elif answer == "no":
+            end()
+        else:
+            yon()
 
-def yon():
-    print("Sorry, you can only answer Yes or No!")
+    while ask < 20:
+        ask += 1
+        Q()
+        answer= input("Are you a cat? ").lower()
+        if answer == "yes":
+            print("Great!!")
+            break
+        elif answer == "no":
+            print("Welp, looks like I\'m a Bear!!")
+            win()
+        else:
+            ask -= 1
+            yon()
 
-def yon2():
-    print("This late in the game and you still can\'t follow the rules!!")
-    print("Looks like you\'ll just have to play again!! :D")
-    R()
+    while ask < 20:
+        ask += 1
+        Q()
+        answer= input("Do I have stripes? ").lower()
+        if answer == "yes":
+            print("Welp, looks like I\'m a Tiger!!")
+            win()
+        elif answer == "no":
+            print("Welp, looks like I\'m a Lion!!")
+            win()
+        else:
+            ask -= 1
+            yon()
 
-while True:
-    print("Welcome!! This is the 20 Questions game!")
-    answer= input("Would you like to play? ").lower()
-    if answer == "yes":
-        print("Great!! Choose between a Lion, Tiger, or Bear!")
-        print("Choose one and I will try and guess what you chose!")
-        break
-    elif answer == "no":
-        end()
-    else:
-        yon()
-
-while True:
-    answer = input("Are you ready to play? ").lower()
-    if answer == "yes":
-        print("Awesome, let\'s get started!!")
-        break
-    elif answer == "no":
-        end()
-    else:
-        yon()
-
-while ask < 20:
-    ask += + 1
-    Q()
-    answer= input("Are you a cat? ").lower()
-    if answer == "yes":
-        print("Great!!")
-        break
-    elif answer == "no":
-        print("Welp, looks like I\'m a Bear!!")
-        win()
-    else:
-        yon()
-
-while ask < 20:
-    ask += + 1
-    Q()
-    answer= input("Do I have stripes? ").lower()
-    if answer == "yes":
-        print("Welp, looks like I\'m a Tiger!!")
-        win()
-    elif answer == "no":
-        print("Welp, looks like I\'m a Lion!!")
-        win()
-    else:
-        ask += - 1
-        yon()
+if __name__ == "__main__":
+    main()
